c20beb34e5ddc1c40c9705c4e7ab30df92bb6b0e
Jun Jung <keyos27@gmail.com>
2020-05-16 13:23:04 -0700

    added a php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+// echo 'Hello, Ninjas';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP File</title>
+</head>
+<body>
+
+    <h1><?php echo 'Hello, Ninjas'; ?></h1>
+    
+</body>
+</html>
