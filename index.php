886223f9139112fc3e0b4f39c119b5c49b352b30
JunJung <keyos27@gmail.com>
2020-05-18 11:58:57 -0700

    changed title

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorials</title>
+    <title>PHP Tutorials | Arrays</title>
 </head>
 <body>
 
