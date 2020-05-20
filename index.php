b9733c6565450b4a9e1760479bcf673bbf0bc998
JunJung <keyos27@gmail.com>
2020-05-19 17:05:56 -0700

    lesson-8

--- index.php
+++ index.php
@@ -4,35 +4,23 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
-    // indexed arrays
-    $peopleOne = ['shaun','crystal','ryu'];    
-    //echo $peopleOne[1];
-    $peopleTwo = array('ken','chun-li');
-    // echo $peopleTwo[1];
-    $ages = [20,30,40,50];
-    // print_r($ages);
-    $ages[1] = 25;
-    // print_r($ages);
-    $ages[] = 60;
-    // print_r($ages);
-    array_push($ages, 70);
-    // print_r($ages);
-    // echo count($ages);
-    $peopleThree = array_merge($peopleOne, $peopleTwo);
-    // print_r($peopleThree);
+    // multi-dimensional arrays
 
+    $blogs = [
+        ['title' => 'mario party','author'=>'mario','content' =>'lorem','likes'=>30],
+        ['title'=>'mario kart cheats','author'=>'toad','content'=>'lorem','likes'=>25],
+        ['title'=>'zelda hidden chest', 'author'=>'link','content'=>'lorem','likes'=>50]
+    ];
 
-    // associative arrays (key & value pairs)
-    $ninjasOne = ['shaun' => 'black','mario' => 'red', 'luigi' => 'green'];
-    // echo $ninjasOne['mario'];
-    // print_r($ninjasOne);
-    $ninjasTwo = array('browser' => 'brown', 'peach' => 'pink','toad' => 'pink');
-    // print_r($ninjasTwo);
-    $ninjasTwo['peach'] = 'yellow';
-    // print_r($ninjasTwo);
-    //echo count($ninjasOne);
-    $ninjasThree = array_merge($ninjasOne, $ninjasTwo);
-    print_r($ninjasThree);
+    // print_r($blogs[1]);
+    // print_r($blogs[1][1]);
+    // echo $blogs[2]['author'];
+    //echo count($blogs);
+    $blogs[] = ['title' => 'castle party','author'=>'peach','content'=>'lorem','likes'=> 100];
+
+    //print_r($blogs);
+    $popped = array_pop($blogs);
+    print_r($popped);
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +28,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorials | Arrays</title>
+    <title>PHP Tutorials | Multi-dimensional Arrays</title>
 </head>
 <body>
 
