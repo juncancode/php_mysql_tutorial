0da7fed1c829ddd1878879e0131adb92e0abb2ca
JunJung <keyos27@gmail.com>
2020-06-02 19:28:29 -0700

    lesson-16

--- index.php
+++ index.php
@@ -4,28 +4,13 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
-    // include('ninjas.php');
-    // require('ninjas.php');
-
-    include'ninjas.php';
-    require'ninjas.php';
-
-    echo 'end of php';
-
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorials | Include & Require</title>
-</head>
-<body>
 
-    <?php include('content.php'); ?>
-    <?php include('content.php'); ?>
-    <?php include('content.php'); ?>
+    <?php include('template/header.php')?>
+    <?php include('template/footer.php')?>
 
 </body>
 </html>
