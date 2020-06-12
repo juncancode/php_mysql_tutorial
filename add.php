aebe10e5f5be6859e79f1c0c51c6a8aa04fab792
JunJung <keyos27@gmail.com>
2020-06-11 19:06:49 -0700

    lesson-20

--- add.php
+++ add.php
@@ -18,21 +18,30 @@
         if(empty($_POST['email'])){
             echo 'An email is required <br />';
         } else {
-            echo htmlspecialchars ($_POST['email']);
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+               echo 'e-mail must be a valid email address';
+            }
         }
 
         // check title
         if(empty($_POST['title'])){
             echo 'A title is required <br />';
         } else {
-            echo htmlspecialchars ($_POST['title']);
+           $title = $_POST['title'];
+           if(!preg_match('/^[a-zA-Z\s]+$/',$title)) {
+            echo 'Title must be letters and spaces only';
+           }
         }
 
-                        // check email
+        // check email
         if(empty($_POST['ingredients'])){
             echo 'An least one ingredient is required <br />';
         } else {
-            echo htmlspecialchars ($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$ingredients)) {
+             echo 'Ingredients must be a comma separated list';
+            }
         }
     } // end of POST check
 
