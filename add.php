0a0afc9f50e8e56ffe4b3f79e56bd10b3be03472
JunJung <keyos27@gmail.com>
2020-06-16 19:13:49 -0700

    lesson-22

--- add.php
+++ add.php
@@ -45,6 +45,13 @@
              $errors['ingredients'] = 'Ingredients must be a comma separated list';
             }
         }
+
+        if(array_filter($errors)){
+            // echo 'errors in the form';
+        } else {
+            // echo 'form is valid';
+            header('Location:index.php');
+        }
     } // end of POST check
 
 ?>
