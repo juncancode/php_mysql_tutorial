fd54d86b3891cc9c9492f1c4a17cb48c0bb55d47
JunJung <keyos27@gmail.com>
2020-06-16 19:59:38 -0700

    lesson-25

--- index.php
+++ index.php
@@ -4,12 +4,37 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
+    // connect to database
+    $conn = mysqli_connect('localhost','shaun','1234','ninja_pizza');
+
+    // check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    // make query & get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+    // free result from memory
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
 
     <?php include('template/header.php')?>
+
     <?php include('template/footer.php')?>
 
 
