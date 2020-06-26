03731b3e14178d742055aea8d4c5152c3cd85c7d
JunJung <keyos27@gmail.com>
2020-06-25 19:32:51 -0700

    lesson-34

--- sandbox.php
+++ sandbox.php
@@ -1,17 +1,16 @@
 <?php 
 
- // ternary operators
- $score = 20;
- 
-//  if($socre > 40){
-//      echo 'high score!';
-//  } else {
-//      echo 'low score :(';
-//  }
 
-// echo $score > 40 ? 'high score!' : 'low score :(';
+// superglobals
 
+// $_GET['name], $_POST['name]
 
+echo $_SERVER['SERVER_NAME'] . '<br/>';
+echo $_SERVER['REQUEST_METHOD'] . '<br/>';
+echo $_SERVER['SCRIPT_FILENAME'] . '<br/>';
+echo $_SERVER['PHP_SELF'] . '<br/>';
+
+// $_SESSION, $_COOKIE
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +21,6 @@
     <title>PHP Tutorials</title>
 </head>
 <body>
-    <p><?php echo $score > 40 ? 'high score!' : 'low score :('; ?></p>
+
 </body>
 </html>
