39fc0818f206cab245054c08802ab0bbdb12f8b2
JunJung <keyos27@gmail.com>
2020-07-03 00:37:49 -0700

    lesson-35

--- sandbox.php
+++ sandbox.php
@@ -5,12 +5,24 @@
 
 // $_GET['name], $_POST['name]
 
-echo $_SERVER['SERVER_NAME'] . '<br/>';
-echo $_SERVER['REQUEST_METHOD'] . '<br/>';
-echo $_SERVER['SCRIPT_FILENAME'] . '<br/>';
-echo $_SERVER['PHP_SELF'] . '<br/>';
+// echo $_SERVER['SERVER_NAME'] . '<br/>';
+// echo $_SERVER['REQUEST_METHOD'] . '<br/>';
+// echo $_SERVER['SCRIPT_FILENAME'] . '<br/>';
+// echo $_SERVER['PHP_SELF'] . '<br/>';
 
 // $_SESSION, $_COOKIE
+
+// sessions
+if(isset($_POST['submit'])) {
+    session_start();
+
+    $_SESSION['name'] = $_POST['name'];
+
+    // echo $_SESSION['name'];
+
+    header('Location:index.php');
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +33,9 @@ echo $_SERVER['PHP_SELF'] . '<br/>';
     <title>PHP Tutorials</title>
 </head>
 <body>
-
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+        <input type="text" name="name">
+        <input type="submit" name="submit" value="submit">
+    </form>
 </body>
 </html>
