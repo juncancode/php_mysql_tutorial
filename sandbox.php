8e98f59d47bf9a6f6be052b42252c6c385bbbb56
JunJung <keyos27@gmail.com>
2020-07-07 19:43:24 -0700

    lesson-38

--- sandbox.php
+++ sandbox.php
@@ -1,47 +1,40 @@
 <?php 
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-// superglobals
+// file system - Part 1
 
-// $_GET['name], $_POST['name]
+// $quotes = readfile('readmee.md');
+// echo $quotes;
 
-// echo $_SERVER['SERVER_NAME'] . '<br/>';
-// echo $_SERVER['REQUEST_METHOD'] . '<br/>';
-// echo $_SERVER['SCRIPT_FILENAME'] . '<br/>';
-// echo $_SERVER['PHP_SELF'] . '<br/>';
+$file = 'readme.md';
 
-// $_SESSION, $_COOKIE
+if(file_exists($file)){
 
-// sessions
-if(isset($_POST['submit'])) {
+    // read file
+    // echo readfile($file) . '<br/>';
 
-    // Cookie for gender
-    setcookie('gender', $_POST['gender'], time() + 86400);
+    // // copy file
+    // copy($file,'quotes.md');
 
-    session_start();
+    // // absoulte path
+    // echo realpath($file) . '<br/>';
 
-    $_SESSION['name'] = $_POST['name'];
+    // // file size
+    // echo filesize($file) . '<br/>';
+    
+    // //rename file
+    // rename($file, 'test.md');
 
-    header('Location:index.php');
+
+
+} else {
+    echo 'file does not exist';
 }
+
+mkdir('quotes');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorials</title>
-</head>
-<body>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-        <input type="text" name="name">
-        <select name="gender" id="">
-            <option value="male">male</option>
-            <option value="female">female</option>
-        </select>
-        <input type="submit" name="submit" value="submit">
-    </form>
-</body>
-</html>
